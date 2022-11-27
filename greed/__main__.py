e84227f135c062714ef56e2523dbd8ba995f8ee4
chigham <chigham@byui.edu>
2022-11-26 21:40:19 -0700

    Publish greed.

---
+++ greed/__main__.py
@@ -0,0 +1,103 @@
+import random
+
+from game.casting.actor import Actor
+from game.casting.artifact import Artifact
+from game.casting.cast import Cast
+from game.casting.score import Score #?
+
+from game.directing.director import Director
+
+from game.services.keyboard_service import KeyboardService
+from game.services.video_service import VideoService
+
+from game.shared.color import Color
+from game.shared.point import Point
+
+
+FRAME_RATE = 70
+MAX_X = 900
+MAX_Y = 600
+CELL_SIZE = 15
+FONT_SIZE = 20
+COLS = 60
+ROWS = 40
+CAPTION = "Greed"
+WHITE = Color(255, 255, 255)
+RED = Color(255, 0, 0)
+DEFAULT_ARTIFACTS = 50 # Test, Original-> 40
+ARTIFACTS_SHAPES = ["|*|","O"]
+VELOCITY = Point(0, 1)
+
+
+def main():
+    
+    # create the cast
+    cast = Cast()
+    
+    # create the banner
+    banner = Actor()
+    banner.set_text("")
+    banner.set_font_size(FONT_SIZE)
+    banner.set_color(WHITE)
+    banner.set_position(Point(CELL_SIZE, 0))
+    cast.add_actor("banners", banner)
+    
+    # create the robot
+    x = int(MAX_X / 2)
+    y = int(MAX_Y - CELL_SIZE) # Mod --------------
+    position = Point(x, y)
+
+    robot = Actor()
+    robot.set_text("#")
+    robot.set_font_size(FONT_SIZE)
+    robot.set_color(RED)
+    robot.set_position(position)
+    cast.add_actor("robots", robot)
+
+    # create the artifacts
+    for _ in range(DEFAULT_ARTIFACTS): # Mod ---
+        text = random.choice(ARTIFACTS_SHAPES) # Mod ----------
+        #message = messages[n] # No message
+
+        x = random.randint(1, COLS - 1)
+        y = random.randint(1, ROWS - 5) # Mod ---------------------
+        position = Point(x, y)
+        position = position.scale(CELL_SIZE)
+
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        color = Color(r, g, b)
+        
+        artifact = Artifact()
+        artifact.set_text(text)
+        artifact.set_font_size(FONT_SIZE)
+        artifact.set_color(color)
+        artifact.set_position(position)
+        artifact.set_velocity(VELOCITY)
+
+        # If the artifact is a "rock" the score will be -1 if the robot touches it # Mod ---
+        if artifact.get_text() == "O":
+            artifact.set_value(-1)
+            #artifact.set_message(message)
+
+        cast.add_actor("artifacts", artifact)
+        
+    
+    """
+    ##### Hello, I commented this because we already have a score in Director class
+    # create initial score    
+    score = Score()
+    score.set_position(Point(MAX_X // 2, 15))
+    score.set_color(RED)
+    cast.add_actor("score", score)"""
+        
+    # start the game
+    keyboard_service = KeyboardService(CELL_SIZE)
+    video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
+    director = Director(keyboard_service, video_service)
+    director.start_game(cast)
+
+
+if __name__ == "__main__":
+    main()
---
+++ greed/game/casting/artifact.py
@@ -0,0 +1,73 @@
+from game.casting.actor import Actor
+import random
+
+
+class Artifact(Actor):
+    """
+    An item of cultural or historical interest. 
+    It may be a gem (good) or a rock (bad).
+    
+    The artifact provides a point if it is a gem, 
+    or takes away a point if it is a rock.
+
+    Attributes:
+        Inherits all attributes of actor.
+        _type (string): It is either a gem or a rock.
+        _value (int): +1 for a gem, -1 for a rock.
+    """
+    def __init__(self):
+        super().__init__()
+        #self._type = random.choice('gem', 'rock')
+        self._value = 1
+
+        """if self._type == 'gem':
+            self._value = 1
+        elif self._type == 'rock':
+            self._value = -1"""
+    
+    def get_value(self):
+        """Gets the artifact's value.
+            
+            Returns:
+                int: The value(+1 or -1).
+        """ 
+        return self._value
+
+    def set_value(self, new_value): # Mod ---
+        """Updates the value to the given one.
+        
+        Args:
+            new_value (int): The given value.
+        """
+        self._value = new_value
+
+'''
+from game.casting.actor import Actor
+
+
+class Artifact(Actor):
+    """
+    An item of cultural or historical interest. 
+    
+    The responsibility of an Artifact is to provide a message about itself.
+
+    Attributes:
+        _message (string): A short description about the artifact.
+    """
+    def __init__(self):
+        super().__init__()
+        self._points = 0
+
+    def set_points(self, points):
+        self.points = points
+    
+    def get_points(self):
+        """Updates the message to the given one.
+        
+        Args:
+            message (string): The given message.
+        """
+        return self._points
+    
+ 
+'''
